923607582462738b41406539765f5b44b7fa8ba2
DoyeonKim <doyeon.kim0208@gmail.com>
2019-11-30 19:15:08 +0900

    Admin Page  using Spring Boot

---
+++ src/main/java/com/example/demo/controller/GetController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+//import org.slf4j.Logger;
+
+import com.example.demo.model.SearchParam;
+
+@RestController
+@RequestMapping("/api")
+public class GetController {
+	@RequestMapping(method = RequestMethod.GET, path = "/getMethod")
+	public String getRequest() {
+		return "Hi getMethod";
+	}
+
+	@GetMapping("/getParameter")
+	public String getParameter(@RequestParam String id, @RequestParam(name = "password") String pwd) {
+		return id + pwd;
+
+	}
+
+	public SearchParam getMultiParameter(SearchParam searchParam) {
+		return searchParam;
+	}
+}
---
+++ src/test/java/com/example/demo/BootjarTestApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootjarTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
